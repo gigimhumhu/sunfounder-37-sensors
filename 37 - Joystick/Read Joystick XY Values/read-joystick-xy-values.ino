87c9fef7dc6913b3442172a8c97caf471e9028d3
Kristy Caster <kristyacaster@gmail.com>
2016-05-13 10:27:38 -0700

    add joystick sketch

--- 37 - Joystick/Read Joystick XY Values/read-joystick-xy-values.ino
+++ 37 - Joystick/Read Joystick XY Values/read-joystick-xy-values.ino
@@ -1,1 +1,20 @@
-/Users/kcaster/Documents/Arduino/joystick/joystick.ino
+/**
+ * Reads and prints the joystick's XY values to the serial monitor
+ */
+
+int xPin = 1;
+int yPin = 0;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  int xVal = analogRead(xPin);
+  int yVal = analogRead(yPin);
+
+  Serial.print(xVal);
+  Serial.print(",");
+  Serial.println(yVal);
+  delay(100);
+}
